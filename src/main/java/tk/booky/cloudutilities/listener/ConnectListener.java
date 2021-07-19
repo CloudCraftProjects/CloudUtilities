bf1a0ed6cdd7e44bcc3e4d9a615c0d20886b917f
booky10 <github@booky.tk>
2021-07-19 15:50:24 +0200

    using server post connect event, this actually works now

--- src/main/java/tk/booky/cloudutilities/listener/ConnectListener.java
+++ src/main/java/tk/booky/cloudutilities/listener/ConnectListener.java
@@ -2,21 +2,13 @@ package tk.booky.cloudutilities.listener;
 // Created by booky10 in CloudUtilities (14:43 18.07.21)
 
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.connection.PluginMessageEvent;
-import com.velocitypowered.api.proxy.Player;
-
-import static tk.booky.cloudutilities.utils.Constants.BRAND_IDENTIFIER;
-import static tk.booky.cloudutilities.utils.Constants.BRAND_MESSAGE;
+import com.velocitypowered.api.event.player.ServerPostConnectEvent;
+import tk.booky.cloudutilities.utils.Constants;
 
 public class ConnectListener {
 
     @Subscribe
-    public void onPluginMessage(PluginMessageEvent event) {
-        if (event.getTarget() instanceof Player) {
-            if (event.getIdentifier().getId().equals(BRAND_IDENTIFIER.getId())) {
-                event.getTarget().sendPluginMessage(BRAND_IDENTIFIER, BRAND_MESSAGE);
-                event.setResult(PluginMessageEvent.ForwardResult.handled());
-            }
-        }
+    public void postConnect(ServerPostConnectEvent event) {
+        event.getPlayer().sendPluginMessage(Constants.BRAND_IDENTIFIER, Constants.BRAND_MESSAGE);
     }
 }
