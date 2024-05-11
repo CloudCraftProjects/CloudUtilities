4febcf70ee194c32dbc73c2e32a99e56d19641e4
booky10 <boooky10@gmail.com>
2024-05-11 04:06:45 +0200

    Initial bukkit plugin setup

---
+++ paper/src/main/java/dev/booky/cloudutilities/bukkit/CloudUtilitiesMain.java
@@ -0,0 +1,12 @@
+package dev.booky.cloudutilities.bukkit;
+// Created by booky10 in CloudUtilities (04:01 11.05.2024.)
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CloudUtilitiesMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Hello World!");
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "CloudUtilities"
 
-listOf("velocity").forEach { name ->
+listOf("velocity", "paper").forEach { name ->
     val projectName = "${rootProject.name}-${name.replaceFirstChar { it.titlecase() }}"
     include(projectName)
     findProject(":${projectName}")!!.projectDir = file(name)
