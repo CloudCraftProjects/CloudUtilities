fa44e3bf8607e34bf00e86f56280bc6f5026d924
booky10 <boooky10@gmail.com>
2024-05-11 04:29:12 +0200

    Implement translation loading for bukkit platform

--- paper/src/main/java/dev/booky/cloudutilities/bukkit/CloudUtilitiesMain.java
+++ paper/src/main/java/dev/booky/cloudutilities/bukkit/CloudUtilitiesMain.java
@@ -1,22 +1,35 @@
 package dev.booky.cloudutilities.bukkit;
 // Created by booky10 in CloudUtilities (04:01 11.05.2024.)
 
+import dev.booky.cloudcore.i18n.CloudTranslator;
 import dev.booky.cloudutilities.bukkit.commands.AbstractCommand;
 import dev.booky.cloudutilities.bukkit.commands.AllowPvPCommand;
 import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 import java.util.List;
+import java.util.Locale;
 
 public class CloudUtilitiesMain extends JavaPlugin {
 
+    private static final List<Locale> SUPPORTED_LOCALES = List.of(
+            Locale.ENGLISH, Locale.GERMAN
+    );
+
+    private @MonotonicNonNull CloudTranslator i18n;
     private @MonotonicNonNull CloudUtilsManager manager;
     private @MonotonicNonNull List<AbstractCommand> commands;
 
     @Override
     public void onLoad() {
+        this.i18n = new CloudTranslator(this.getClassLoader(),
+                new NamespacedKey(this, "i18n"),
+                SUPPORTED_LOCALES);
+        this.i18n.load();
+
         this.manager = new CloudUtilsManager(this);
         Bukkit.getServicesManager().register(CloudUtilsManager.class,
                 this.manager, this, ServicePriority.Normal);
@@ -34,10 +47,15 @@ public class CloudUtilitiesMain extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        for (AbstractCommand command : this.commands) {
-            command.unregister();
+        if (this.commands != null) {
+            for (AbstractCommand command : this.commands) {
+                command.unregister();
+            }
+            this.commands.clear();
         }
-        this.commands.clear();
+        if (this.i18n != null) {
+            this.i18n.unload();
+        }
     }
 
     public CloudUtilsManager getManager() {
