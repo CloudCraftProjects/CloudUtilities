59b44c2a96c5d2b932a200912eabf9085934b481
booky10 <boooky10@gmail.com>
2024-10-02 19:08:40 +0200

    Add foojay toolchain build plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,9 @@
 rootProject.name = "CloudUtilities"
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 listOf("velocity", "paper").forEach { name ->
     val projectName = "${rootProject.name}-${name.replaceFirstChar { it.titlecase() }}"
     include(projectName)
