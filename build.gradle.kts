ba298ca1dc0766ff2d72a7c6ba7cc6085c511913
booky10 <boooky10@gmail.com>
2024-10-02 19:39:15 +0200

    Add auto publishing gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 import com.github.jengelman.gradle.plugins.shadow.ShadowPlugin
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import me.modmuss50.mpp.ModPublishExtension
+import me.modmuss50.mpp.MppPlugin
+import me.modmuss50.mpp.PublishModTask
 import net.kyori.indra.IndraPlugin
 import org.jetbrains.gradle.ext.IdeaExtPlugin
+import java.nio.file.Files
 
 plugins {
     id("java-library")
@@ -15,32 +19,11 @@ plugins {
     alias(libs.plugins.blossom) apply false
 
     alias(libs.plugins.shadow) apply false
+    alias(libs.plugins.publishing)
 }
 
 tasks.withType<Jar> {
     enabled = false
-}
-
-allprojects {
-    apply<IndraPlugin>()
-
-    group = "dev.booky"
-
-    repositories {
-        maven("https://repo.cloudcraftmc.de/public/")
-    }
-
-    indra {
-        javaVersions {
-            target(21)
-        }
-    }
-
-    java {
-        toolchain {
-            vendor = JvmVendorSpec.ADOPTIUM
-        }
-    }
 }
 
 subprojects {
@@ -64,6 +47,87 @@ subprojects {
 
         assemble {
             dependsOn(project.tasks.withType<ShadowJar>())
+        }
+    }
+}
+
+allprojects {
+    apply<IndraPlugin>()
+    apply<MppPlugin>()
+
+    group = "dev.booky"
+
+    repositories {
+        maven("https://repo.cloudcraftmc.de/public/")
+    }
+
+    indra {
+        javaVersions {
+            target(21)
+        }
+    }
+
+    java {
+        toolchain {
+            vendor = JvmVendorSpec.ADOPTIUM
+        }
+    }
+
+    configure<ModPublishExtension> {
+        val repositoryName = "CloudCraftProjects/CloudUtilities"
+        changelog = "See https://github.com/$repositoryName/releases/tag/v${project.version}"
+        type = if (project.version.toString().endsWith("-SNAPSHOT")) BETA else STABLE
+        dryRun = !hasProperty("noDryPublish")
+
+        if (project != rootProject) {
+            file = tasks.named<Jar>("shadowJar").flatMap { it.archiveFile }
+            additionalFiles.from(tasks.sourcesJar.flatMap { it.archiveFile })
+        } else {
+            // this sadly needs a dummy file for github release parenting to properly work
+            file = rootProject.layout.buildDirectory.file("empty.txt")
+            if (Files.notExists(file.asFile.get().toPath())) {
+                Files.createFile(file.asFile.get().toPath())
+            }
+        }
+
+        github {
+            accessToken = providers.environmentVariable("GITHUB_API_TOKEN")
+                .orElse(providers.gradleProperty("ccGithubToken"))
+
+            displayName = "${rootProject.name} v${project.version}"
+
+            repository = repositoryName
+            commitish = "master"
+            tagName = "v${project.version}"
+
+            if (project != rootProject) {
+                parent(rootProject.tasks.named("publishGithub"))
+            }
+        }
+        if (rootProject != project) {
+            modrinth {
+                accessToken = providers.environmentVariable("MODRINTH_API_TOKEN")
+                    .orElse(providers.gradleProperty("ccModrinthToken"))
+
+                val platformName = project.projectDir.name
+                val fancyPlatformName = platformName.replaceFirstChar { it.titlecaseChar() }
+                version = "${project.version}+$platformName"
+                displayName = "${rootProject.name} $fancyPlatformName v${project.version}"
+                modLoaders.add(platformName)
+
+                projectId = "dGVhuvFX"
+                minecraftVersionRange {
+                    start = rootProject.libs.versions.paper.get().split("-")[0]
+                    end = "latest"
+                }
+            }
+        }
+    }
+
+    if (rootProject != project) {
+        tasks.withType<PublishModTask> {
+            dependsOn(tasks.named<Jar>("shadowJar"))
+            dependsOn(tasks.sourcesJar)
         }
     }
 }
